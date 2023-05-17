2f0d3bc93dc7b156becb732f9b2dd565166f0cec
JrzenonDev <jrobertoonb@gmail.com>
2023-05-17 10:12:40 -0300

    feat: configure font in project web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50">
       <h1>Time Line</h1>
+      <h1 className="font-alt">Time Line</h1>
     </div>
   )
 }
