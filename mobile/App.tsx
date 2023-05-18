3c15466061697eb3d59c145e071de1769ebf2e07
JrzenonDev <jrobertoonb@gmail.com>
2023-05-18 11:32:50 -0300

    feat: configure font in project mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,27 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  // Só aprensenta algo em tela depois do carregamento das fonts
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-gray-900 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-6xl">Hello!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-alt text-6xl font-bold text-zinc-50">Hello!</Text>
       <StatusBar style="light" translucent />
     </View>
   )
