084fcf623d884fd69ab8afef79281dd7d98ba1cc
JrzenonDev <jrobertoonb@gmail.com>
2023-05-18 11:33:13 -0300

    feat: adjusts in prettier e lint

--- mobile/prettier.config.js
+++ mobile/prettier.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        parser: "flow",
+      },
+    ],
+  },
 };
