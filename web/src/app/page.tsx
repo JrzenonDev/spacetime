6e1f9e5fef5e2ca703a9001a2b0ce8b3e57ebcdf
JrzenonDev <jrobertoonb@gmail.com>
2023-05-18 09:30:09 -0300

    feat: create sections signin, hero, copyright

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,6 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import spacetimeLogo from '../assets/spacetime-log.svg'
 
 export default function Home() {
   return (
@@ -5,10 +8,38 @@ export default function Home() {
 
       {/* Left */}
       <div className="relative flex flex-col item-start justify-between px-28 py-16 overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
         {/* Stripes */}
         <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a href="#" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className='h-5 w-5 text-gray-500' />
+          </div>
+          <p className='text-sm leading-snug max-w-[140px]'><span className='underline'>Crie sua conta</span> e salve suas memórias!</p>
+        </a>
+
+        {/* Hero */}
+        <div className='space-y-5'>
+          <Image src={spacetimeLogo} alt="Space time logo" />
+
+          <div className='max-w-[420px] space-y-4'>
+            <h1 className='text-5xl font-bold leading-tight text-gray-50'>Sua linha do tempo</h1>
+            <p className='text-lg leading-relaxed'>Colecione momentos marcantes da sua jornada e compatilhe (se quiser) com o mundo!</p>
+          </div>
+
+          <a href='' className='uppercase inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm leading-none text-black hover:bg-green-600'>Cadastrar lembrança</a>
+        </div>
+
+        {/* Copyright */}
+        <div className='text-sm leading-relaxed text-gray-50'>
+          Feito com ❤️ <a href='https://github.com/JrzenonDev' target='_blank' rel='noopener' className='underline hover:text-gray-200'>JrzenonDev</a>
+        </div>
+
       </div>
 
       {/* Right */}
