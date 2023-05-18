fe0ad8828b1502a221861cd0ce606f8455ca7bb0
JrzenonDev <jrobertoonb@gmail.com>
2023-05-18 14:03:48 -0300

    feat: complete home page mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,10 +5,16 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { styled } from 'nativewind'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import blugBg from './src/assets/bg-blur.png'
+import SpacetimeLogo from './src/assets/spacetime-log.svg'
+import Stripes from './src/assets/stripes.svg'
 
 export default function App() {
+  // por padrão a lib nativewind só estiliza componentes nativos do react-native, por isso usamos essa abordagem abaixo
+  const StyledStripes = styled(Stripes)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -23,9 +29,37 @@ export default function App() {
   return (
     <ImageBackground
       source={blugBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <SpacetimeLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua linha do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos da sua jornada e compartilhe (se quiser) com o
+            mundo
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com ❤️ JrzenonDev
+      </Text>
+
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
