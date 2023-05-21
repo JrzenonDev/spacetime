17e131e1b27787ef2917e7d75ffa2c9cda4cff15
JrzenonDev <jrobertoonb@gmail.com>
2023-05-21 11:55:12 -0300

    feat: create component Copyright

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,7 @@
+export function Copyright() {
+  return(
+    <div className='text-sm leading-relaxed text-gray-50'>
+      Feito com ❤️ <a href='https://github.com/JrzenonDev' target='_blank' rel='noopener' className='underline hover:text-gray-200'>JrzenonDev</a>
+    </div>
+  )
+}
