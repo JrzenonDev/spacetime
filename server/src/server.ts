57ed9b0540ce39ae170b7ac30433b590ae680100
JrzenonDev <jrobertoonb@gmail.com>
2023-05-22 01:00:17 -0300

    feat: use route auth

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,16 @@
 import cors from '@fastify/cors'
+import 'dotenv/config'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/menories'
 
 const app = fastify()
 
 app.register(cors, {
-  // origin: ['http://localhost: 300'. 'https:seu-provedor']
+  // origin: ['http://localhost: 300'. 'https://seu-provedor.com.br/']
   origin: true,
 })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
